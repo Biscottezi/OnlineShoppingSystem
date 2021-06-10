dc0124b63bbf9fd6a5b0572af528953c6bc61036
Biscottezi <nguyenphamminhquan7516@gmail.com>
2021-06-10 14:02:01 +0700

    update parameter name for homepage.jsp

---
+++ build/web/js/homepage.js
@@ -0,0 +1,31 @@
+function openLogin(){
+    if(document.getElementById("register").style.display == "block"){
+        closePopUp("register");
+    }
+    if(document.getElementById("forgot-password").style.display == "block"){
+        closePopUp("forgot-password");
+    }
+    document.getElementById('login').style.display = "block";
+}
+
+function closePopUp(eleId){
+    document.getElementById(eleId).style.display = "none";
+}
+
+function openRegister(){
+    if(document.getElementById("login").style.display == "block"){
+        closePopUp("login");
+    }
+    document.getElementById('register').style.display = "block";
+}
+
+function openForgotPassword(){
+    if(document.getElementById("login").style.display == "block"){
+        closePopUp("login");
+    }
+    document.getElementById('forgot-password').style.display = "block";
+}
+
+function resetNoti(){
+    alert("Please check your email inbox. Thank you!");
+}
