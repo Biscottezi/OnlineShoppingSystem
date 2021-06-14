1cb1547ed251a675d3f2b54eba1ff6ab894b6a6e
LamDoHoang <irokatsuna@gmail.com>
2021-06-14 13:54:25 +0700

    modify viewHomePageServlet

--- src/java/controller/viewHomePageServlet.java
+++ src/java/controller/viewHomePageServlet.java
@@ -7,11 +7,19 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+import javax.naming.NamingException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import post.PostDAO;
+import post.PostDTO;
+import product.ProductDAO;
+import product.ProductDTO;
 
 /**
  *
@@ -19,7 +27,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "viewHomePageServlet", urlPatterns = {"/viewHomePageServlet"})
 public class viewHomePageServlet extends HttpServlet {
-
+    private final String ERROR_PAGE = "";
+    private final String HOME_PAGE = "";
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -32,17 +41,33 @@ public class viewHomePageServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        String url = ERROR_PAGE;
+        
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet viewHomePageServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet viewHomePageServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            ProductDAO productDao = new ProductDAO();
+            productDao.getFeaturedProduct();
+            List<ProductDTO> productList = productDao.getProductList();
+            if(productList != null){
+                request.setAttribute("FEATURED_PRODUCT", productList);
+            }
+            
+            PostDAO postDao = new PostDAO();
+            postDao.getFeaturedPost();
+            List<PostDTO> postList = postDao.getPostList();
+            if(postList != null){
+                request.setAttribute("FEATURED_POST", postList);
+            }
+            
+            
+            
+            url = HOME_PAGE;
+        }catch(SQLException ex){
+            log("showProductServlet _ SQL:" + ex.getMessage());
+        }catch(NamingException ex){
+            log("showProductServlet _ Naming:" + ex.getMessage());
+        }finally{
+            RequestDispatcher rd = request.getRequestDispatcher(url);
+            rd.forward(request, response);
         }
     }
 
