54242168f8779b21d87389ac1725567a510c9d79
Nesto <tanlong6121@gmail.com>
2021-06-17 20:34:37 +0700

    upload userlist.jsp

---
+++ build/web/js/managerpopup.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function showPopup() {
+  document.getElementById("usermenu").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.ava')) {
+    var dropdowns = document.getElementsByClassName("popupwrapper");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
